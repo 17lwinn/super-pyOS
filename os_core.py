86c62f77c664394714ab937a156f722df67107b1
Glitch (hello-webpage) <none>
2020-10-04 16:20:49 +0000

    ⛑🍅 Checkpoint ./os_core.py:5152800/1004

--- os_core.py
+++ os_core.py
@@ -4,4 +4,9 @@ from time import *
 
 def startup():
   print("Beginning startup proccedure...")
-  
+  import importlib
+  try:
+    importlib.import_module("os_handle")
+  except ImportError:
+    print("\033[1;32;40m OS_HANDLER not found, aborting...")
+    sys.exit("Error 1A: OS_HANDLER NOT FOUND: \n\n This occurs when the OS command handler could not be found, either re-download the module/OS or try moving to this folder")
