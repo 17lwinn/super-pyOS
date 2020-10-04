32b130d00e4a8e723d612404a1339226f44755dd
Glitch (hello-webpage) <none>
2020-10-04 16:32:19 +0000

    💋👕 Checkpoint ./os_core.py:5152800/2769

--- os_core.py
+++ os_core.py
@@ -3,10 +3,24 @@ import sys
 from time import *
 
 def startup():
+  def moduleCheck():
+    import importlib
+    try:
+      importlib.import_module("os_handler")
+    except ImportError:
+      print("\033[1;37;41m" + "OS_HANDLER not found, aborting...")
+      sys.exit("Error 1A: OS_HANDLER NOT FOUND: \n\nThis occurs when the OS command handler could not be found, either re-download the module/OS or try moving to this folder."  + "\033[0m")
+    else:
+      print("\033[1;37;42m" + " OS_HANDLER found " + "\033[0m")
+    try:
+      importlib.import_module("main")
+    except ImportError:
+      print("\033[1;37;41m" + "MAIN not found, aborting...")
+      sys.exit("Error 1A: MAIN NOT FOUND: \n\nThis occurs when the main OS could not be found, either re-download the module/OS or try moving to this folder."  + "\033[0m")
+    else:
+      print("\033[1;37;42m" + " MAIN found " + "\033[0m")
   print("Beginning startup proccedure...")
-  import importlib
-  try:
-    importlib.import_module("os_handle")
-  except ImportError:
-    print("\033[1;32;40m OS_HANDLER not found, aborting...")
-    sys.exit("Error 1A: OS_HANDLER NOT FOUND: \n\n This occurs when the OS command handler could not be found, either re-download the module/OS or try moving to this folder")
+  print("Step I, Finding required modules...")
+  sleep(2)
+  moduleCheck()
+  
