409eb109818b0a58d4c8c28464ede80a45b4a811
Glitch (hello-webpage) <none>
2020-10-04 16:44:07 +0000

    🚡👽 Checkpoint ./os_core.py:5152800/4093 ./main.py:5152800/623

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import os_handler as command
 import os_core as core
+import importlib
+import sys
+
+try:
+  importlib.import_module("os_core")
+except ImportError:
+    print("OS_CORE not found, aborting...")
+    sys.exit("\033[1;37;41m" + "Error 1C: OS_CORE NOT FOUND: \n\nThis occurs when the OS core could not be found, either re-download the module/OS or try moving to this folder."  + "\033[0m")
+else:
+  print("")
 
 core.startup()
 
