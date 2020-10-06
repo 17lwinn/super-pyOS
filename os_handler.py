ebce09a250a9bc9f5fef43232d37bb7c595161c8
Glitch (hello-webpage) <none>
2020-10-06 06:43:50 +0000

    🏈🌷 Checkpoint ./example.py:5152800/18 ./os_services.py:5152800/115 ./os_handler.py:5152800/1811

--- os_handler.py
+++ os_handler.py
@@ -4,6 +4,21 @@ import sys
 # command handler for the OS
 
 def executeCommand(command):
+  if command == "makeService":
+    name = input("filename without .py: ")
+    print("checking code can be imported...")
+    try:
+      importlib.import_module(name)
+    except ImportError:
+      print("not found, aborting...")
+    try:
+      print("checking init function is available..."):
+      service = importlib.import_module(name)
+      service.init()
+    except NameError:
+      print("init not found, aborting...")
+
+    
   if command == "test":
     print("hiya")
   if command == "ys":
--- os_services.py
+++ os_services.py
@@ -6,3 +6,5 @@ import os
 #--------------------------------------------------------------------------
 # SUPERPYOS SYSTEM SERVICES FILE, SEE API/CORE.MD ON ADDING A SERVICE
 # add them below this line
+
+# core.registerService("package")
