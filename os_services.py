b4587d4bb971eb86c0a3937e15d4e8ed25293328
Glitch (superpy-2-0) <none>
2020-10-06 18:55:45 +0000

    🐫📒 Checkpoint ./os_services.py:2201711/195 ./os_core.py:2201711/206

--- os_services.py
+++ os_services.py
@@ -3,8 +3,22 @@ import importlib
 import sys
 import os
 
+
+
+
+
 #--------------------------------------------------------------------------
 # SUPERPYOS SYSTEM SERVICES FILE, SEE API/CORE.MD ON ADDING A SERVICE
+#--------------------------------------------------------------------------
+# Core Services
+
+
+
+
+
+
+
+#--------------------------------------------------------------------------
 # add them below this line
 
 # core.registerService("package")
