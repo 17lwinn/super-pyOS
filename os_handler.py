1479c745b7ab6f7bdbfb90e80235f28e6bd37010
Glitch (hello-webpage) <none>
2020-10-06 06:18:32 +0000

    🚀🍈 Checkpoint ./os_handler.py:5152800/301

--- os_handler.py
+++ os_handler.py
@@ -7,8 +7,7 @@ def executeCommand(command):
   if command == "test":
     print("hiya")
   if command == "ys":
-    print("Want to use YoungShell?");
-    #Install command idk much python
-    #git clone https://github.com/youngchief-btw/YoungShell.git;
-    #cd YoungShell;
-    #bash src/PortableSetup.sh
+    ys = input("install youngshell? y/n: ")
+    
+    if ys == "y":
+      print("installing modules for cloning: ")
