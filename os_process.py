eda582b4406761470de818f81b9f47e2cbfb196a
Glitch (superpy-2-0) <none>
2020-10-06 19:18:55 +0000

    👂🏮 Checkpoint ./os_process.py:2201711/523

--- os_process.py
+++ os_process.py
@@ -4,10 +4,15 @@ import os_handler as command
 
 #reserved process ids are process ids used by the system, if a process that is being made tries to take one of these ids 
 #without some sort of identification that they are from the system, the process will be ended before it could cause damage to the system 
-ReservedProcessIds = range(0,3)
-print(ReservedProcessIds)
+reservedProcessIds = list(range(1,5))
 
-class Process:
-  def __init__(self, name, process, ID):
+class process:
+  name = ""
+  
+  def __init__(self, name, proc, ID):
     self.name = name
-    
+    for reservedId in reservedProcessIds:
+      if reservedId == ID:
+        del self.name
+x = process("a", "b", 1)
+print(x.name)
