f771568e97ae882d808ff3869448b4f3747d1af7
Glitch (superpy-2-0) <none>
2020-10-07 06:37:09 +0000

    🐲🎍 Checkpoint ./os_core.py:5152800/277

--- os_core.py
+++ os_core.py
@@ -4,13 +4,14 @@ import os
 import sys
 from time import *
 import importlib
-import threading
 
 def registerService(service):
   try:
     importlib.import_module(service)
   except ModuleNotFoundError:
     print("\033[1;37;41m" + " Error 1D: ServiceNotFound: " + service + " \033[0m")
+  except TypeError:
+    print("\033[1;37;41m" + " Error ?: " +  + " \033[0m")
   else:  
     service = importlib.import_module(service)
     service.init()
